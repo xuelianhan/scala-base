e3e174f8223de885661992b4795384edd3bf7711
xuelian.han <xuelian.han@qunar.com>
2015-07-26 15:07:34 +0800

    add answers

---
+++ src/main/scala/cn/ict/base/sfti/Chapter1.scala
@@ -0,0 +1,16 @@
+package cn.ict.base.sfti
+/**
+ * [hanxuelian@ict scala]$ scalac  ./cn/ict/base/sfti/Chapter1.scala 
+ * [hanxuelian@ict scala]$ scala  cn.ict.base.sfti.Chapter1
+ * crazycrazycrazy
+ * [hanxuelian@ict scala]$ pwd
+ * /home/hanxuelian/Public/scala-base/src/main/scala
+ *
+ */
+
+
+
+object Chapter1 extends App {
+  println("crazy"*3)
+
+}

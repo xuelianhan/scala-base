efd859f3dc5f5447f619d48c3aa2519462d00889
xuelian.han <xuelian.han@qunar.com>
2015-07-29 10:04:52 +0800

    add answers of Chapter1

--- src/main/scala/cn/ict/base/sfti/Chapter1.scala
+++ src/main/scala/cn/ict/base/sfti/Chapter1.scala
@@ -12,7 +12,17 @@ package cn.ict.base.sfti
 import BigInt.probablePrime
 import util.Random
 object Chapter1 extends App {
-  println("crazy"*3)
-  println(BigInt(2).pow(1024))
-  println(probablePrime(100,Random)) 
+  println("crazy*3:"+"crazy"*3)
+  println("BigInt(2).pow(1024):"+BigInt(2).pow(1024))
+  println("probablePrime(100, Random):"+probablePrime(100,Random)) 
+  println("BigInt.apply(Random.nextInt).toString(36):"+BigInt.apply(Random.nextInt).toString(36))
+  
+  var hello : String = "Hello, World!"
+  println("hello content:"+hello)
+  println("hello.head:"+hello.head)
+  println("hello.last:"+hello.last)
+  println("hello.take(1):"+hello.take(1))
+  println("hello.takeRight(1):"+hello.takeRight(1))
+  println("hello.drop(6):"+hello.drop(6))
+  println("hello.dropRight(7)"+hello.dropRight(7))
 }

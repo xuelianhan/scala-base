961612969724d3f222b12907ea0d6e2a332ac4a3
Randy Hubbard <randyhubbard@Randys-MacBook-Pro.local>
2022-11-28 14:14:42 +0800

    add codes

---
+++ src/main/cn/ict/base/HelloWord.scala
@@ -0,0 +1,12 @@
+package cn.ict.base
+
+/**
+ * @author sniper
+ * @date 28 Nov, 2022 
+ */
+object HelloWord {
+
+    def main(args: Array[String]): Unit = {
+        println("Hello, World!")
+    }
+}
--- src/main/scala/cn/ict/base/sfti/Chapter2.scala
+++
@@ -1,9 +0,0 @@
-package cn.ict.base.sfti
-
-
-//http://www.cs.cmu.edu/~epxing/Class/10708-14/lecture.html
-object Chapter2 extends App {
-   
-
-
-}

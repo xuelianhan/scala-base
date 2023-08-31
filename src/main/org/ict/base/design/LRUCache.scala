5742d54fb33824354362ac84b1f2cc0266c593f4
xueliansniper <xueliansniper@gmail.com>
2023-08-31 16:35:48 +0800

    add codes

---
+++ src/main/org/ict/base/design/LRUCache.scala
@@ -0,0 +1,22 @@
+package org.ict.base.design
+
+/**
+ * @author sniper
+ * @date 31 Aug 2023
+ * LC146, Medium
+ */
+object LRUCache {
+
+    class LRUCache(_capacity: Int) {
+
+      def get(key: Int): Int = {
+          return null
+      }
+
+      def put(key: Int, value: Int) {
+
+      }
+
+    }
+
+}

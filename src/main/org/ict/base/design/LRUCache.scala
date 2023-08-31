acfee17f180f43228f6039eeeb732f01702c75fd
xueliansniper <xueliansniper@gmail.com>
2023-08-31 16:45:31 +0800

    add codes

--- src/main/org/ict/base/design/LRUCache.scala
+++ src/main/org/ict/base/design/LRUCache.scala
@@ -3,19 +3,19 @@ package org.ict.base.design
 /**
  * @author sniper
  * @date 31 Aug 2023
- * LC146, Medium
+ * LC146, Medium, frequency=286
  */
 object LRUCache {
 
     class LRUCache(_capacity: Int) {
 
-      def get(key: Int): Int = {
-          return null
-      }
+        def get(key: Int): Int = {
+            return null
+        }
 
-      def put(key: Int, value: Int) {
+        def put(key: Int, value: Int) {
 
-      }
+        }
 
     }
 
